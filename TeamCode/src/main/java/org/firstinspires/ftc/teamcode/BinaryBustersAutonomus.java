b0e20f71f65b190646e18e3f909f0d8a1f310a1b
yearbook <caleb.andrew.flaim@gmail.com>
2017-10-11 16:22:39 -0500

    We started Septeleop, a test file, and BinaryBustersTeleOp, our official code. Septeleop has our basic teleop code. BinaryBusterTeleOp is empty.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BinaryBustersAutonomus.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by yearbook on 10/11/17.
+ */
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+public class BinaryBustersAutonomus {
+    public DcMotor backRightMotor;
+    public DcMotor backLeftMotor;
+
+    public DcMotor glyphVertical;
+    public Servo glyphHorizontal;
+
+    public DcMotor relicHorizontal;
+
+    public void main() {
+
+    }
+}
